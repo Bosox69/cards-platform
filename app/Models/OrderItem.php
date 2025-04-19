78af8194383b55d81df8b4c1740ca4a57e37fc19
Bosox69 <arnaud.saumier@gmail.com>
2025-04-19 21:35:00 +0200

    Update OrderItem.php

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -5,17 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-Schema::create('order_items', function (Blueprint $table) {
-    $table->id();
-    $table->foreignId('order_id')->constrained()->cascadeOnDelete();
-    $table->foreignId('template_id')->constrained();
-    $table->foreignId('department_id')->constrained();
-    $table->integer('quantity');
-    $table->boolean('is_double_sided')->default(false);
-    $table->string('pdf_preview')->nullable();
-    $table->timestamps();
-});
-
 class OrderItem extends Model
 {
     use HasFactory;
@@ -43,6 +32,6 @@ class OrderItem extends Model
     
     public function cardData()
     {
-        return $this->hasMany(CardData::class);
+        return $this->hasOne(CardData::class);
     }
 }
